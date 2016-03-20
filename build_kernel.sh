66de78b0540fef1fa7052155b781a501a17d9aa5
robcore <robpatershuk@gmail.com>
2016-03-20 06:51:23 -0600

    minor build script mod

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ make clean
 make mrproper
 mkdir $(pwd)/out
 cp $(pwd)/arch/arm/configs/AEL_config $(pwd)/out/.config
-make ARCH=arm -j7 -C $(pwd) O=$(pwd)/out silentoldconfig
+make ARCH=arm -j7 -C $(pwd) O=$(pwd)/out oldconfig
 make ARCH=arm -j7 -C $(pwd) O=$(pwd)/out
 cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
 cp $(pwd)/out/drivers/net/wireless/bcmdhd/dhd.ko $(pwd)/arch/arm/boot/dhd.ko
