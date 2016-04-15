dd0f9fa490f0589a754d20158d73453d2f7b0414
robcore <robpatershuk@gmail.com>
2016-04-14 19:33:50 -0600

    From 8eaba4c91c87f5cc01e70b52c26fe6637078faac Mon Sep 17 00:00:00 2001 From: Slim80 <bestboss1980@gmail.com> Date: Thu, 14 Jan 2016 08:17:39 +0100 Subject: [PATCH] ARM: optimize memset_io()/memcpy_fromio()/memcpy_toio()
    ---
     arch/arm/include/asm/io.h | 25 +++++++++++++++++++++++++
     1 file changed, 25 insertions(+)
    
    diff --git a/arch/arm/include/asm/io.h b/arch/arm/include/asm/io.h
    index 42fef7c..9b15cc9 100755
    --- a/arch/arm/include/asm/io.h
    +++ b/arch/arm/include/asm/io.h
    @@ -23,6 +23,7 @@
    
     #ifdef __KERNEL__
    
    +#include <linux/string.h>
     #include <linux/types.h>
     #include <asm/byteorder.h>
     #include <asm/memory.h>
    @@ -291,9 +292,33 @@ extern void _memset_io(volatile void __iomem *, int, size_t);
     #define writesw(p,d,l)		__raw_writesw(p,d,l)
     #define writesl(p,d,l)		__raw_writesl(p,d,l)
    
    +#ifndef __ARMBE__
    +static inline void memset_io(volatile void __iomem *dst, unsigned c,
    +	size_t count)
    +{
    +	memset((void __force *)dst, c, count);
    +}
    +#define memset_io(dst,c,count) memset_io(dst,c,count)
    +
    +static inline void memcpy_fromio(void *to, const volatile void __iomem *from,
    +	size_t count)
    +{
    +	memcpy(to, (const void __force *)from, count);
    +}
    +#define memcpy_fromio(to,from,count) memcpy_fromio(to,from,count)
    +
    +static inline void memcpy_toio(volatile void __iomem *to, const void *from,
    +	size_t count)
    +{
    +	memcpy((void __force *)to, from, count);
    +}
    +#define memcpy_toio(to,from,count) memcpy_toio(to,from,count)
    +
    +#else
     #define memset_io(c,v,l)	_memset_io(c,(v),(l))
     #define memcpy_fromio(a,c,l)	_memcpy_fromio((a),c,(l))
     #define memcpy_toio(c,a,l)	_memcpy_toio(c,(a),(l))
    +#endif
    
     #endif	/* readl */

--- intelli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://github.com/shminer/android_kernel_lge_f460/commit/6a9cce24693a58eaadbd028c307c7ea4072360e5.patch;
-wget https://github.com/shminer/android_kernel_lge_f460/commit/4927567218258f4e8c4160eee6fd9badb3cbd4d0.patch;
-wget https://github.com/shminer/android_kernel_lge_f460/commit/c500fa800d3ef0006924b652386dbd6e86def715.patch;
-wget https://github.com/shminer/android_kernel_lge_f460/commit/c519da6bbafed5e675fba7bd22e1723186ff0180.patch;
-wget https://github.com/shminer/android_kernel_lge_f460/commit/4eff5a02366057f0576c14a2263d2a04d0739ffa.patch;
-wget https://github.com/shminer/android_kernel_lge_f460/commit/1b787b00326d13c58e94e3e02a0fd1779b18536f.patch;
-wget https://github.com/shminer/android_kernel_lge_f460/commit/c7539517e38f5ada74d06c77abd1605f4b363487.patch;
-pp 6a9cce24693a58eaadbd028c307c7ea4072360e5.patch && pp 4927567218258f4e8c4160eee6fd9badb3cbd4d0.patch && pp c500fa800d3ef0006924b652386dbd6e86def715.patch && pp c519da6bbafed5e675fba7bd22e1723186ff0180.patch && pp 4eff5a02366057f0576c14a2263d2a04d0739ffa.patch && pp 1b787b00326d13c58e94e3e02a0fd1779b18536f.patch && pp c7539517e38f5ada74d06c77abd1605f4b363487.patch
