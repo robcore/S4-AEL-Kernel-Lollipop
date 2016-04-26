83ec1009b295ff615ee05767b0571d9300b1d53c
robcore <robpatershuk@gmail.com>
2016-04-26 06:44:11 -0600

    actually disable media tuner-my bad

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Apr 26 06:42:22 MDT 2016"
+#define LINUX_COMPILE_BY "robcore"
+#define LINUX_COMPILE_HOST "machina"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.06 - Cortex-A15) "
