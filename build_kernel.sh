98a287a2cf6d9ca4d55cf1359d150064989a8aa2
robcore <robpatershuk@gmail.com>
2016-04-26 09:59:16 -0600

    updated build script, testing out kcal, 5.9

--- build_kernel.sh
+++ build_kernel.sh
@@ -14,3 +14,5 @@ make ARCH=arm -j7 -C $(pwd) O=$(pwd)/out;
 cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage;
 cp $(pwd)/out/drivers/net/wireless/bcmdhd/dhd.ko $(pwd)/arch/arm/boot/dhd.ko;
 cp $(pwd)/out/drivers/scsi/scsi_wait_scan.ko $(pwd)/arch/arm/boot/scsi_wait_scan.ko;
+cp $(pwd)/out/drivers/gpu/kcontrol_module/kcontrol_gpu_msm.ko $(pwd)/arch/arm/boot/kcontrol_gpu_msm.ko;
+mv $(pwd)/arch/arm/boot/zImage $(pwd)/arch/arm/boot/boot.img-zImage
