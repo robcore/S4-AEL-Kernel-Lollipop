8cb66e1c9334d219b1b418ab5beddde39791f451
robcore <robpatershuk@gmail.com>
2016-04-26 15:07:59 -0600

    revert kcal, pretty useless on our device

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,7 @@ rm -rf $(pwd)/out;
 rm $(pwd)/arch/arm/boot/dhd.ko;
 rm $(pwd)/arch/arm/boot/scsi_wait_scan.ko;
 rm $(pwd)/arch/arm/boot/zImage;
+rm $(pwd)/arch/arm/boot/boot.img-zImage;
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06/bin/arm-cortex_a15-linux-gnueabihf-
 make clean;
