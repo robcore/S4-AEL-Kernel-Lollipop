8ce907c2790923366190ccc9aec440ca89cc395c
robcore <robpatershuk@gmail.com>
2016-04-28 12:23:54 -0600

    disable boeffla for now, does not boot-also some other reverts for constistency

--- build_kernel.sh
+++ build_kernel.sh
@@ -15,4 +15,11 @@ make ARCH=arm -j7 -C $(pwd) O=$(pwd)/out;
 cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage;
 cp $(pwd)/out/drivers/net/wireless/bcmdhd/dhd.ko $(pwd)/arch/arm/boot/dhd.ko;
 cp $(pwd)/out/drivers/scsi/scsi_wait_scan.ko $(pwd)/arch/arm/boot/scsi_wait_scan.ko;
-mv $(pwd)/arch/arm/boot/zImage $(pwd)/arch/arm/boot/boot.img-zImage
+mv $(pwd)/arch/arm/boot/zImage $(pwd)/arch/arm/boot/boot.img-zImage;
+cp /media/root/robcore/AIK/S4-Machinex-6.x /media/root/robcore/AIK/S4-Machinex-6.x-new;
+cp $(pwd)/arch/arm/boot/dhd.ko /media/root/robcore/AIK/S4-Machinex-6.x-new/system/lib;
+cp $(pwd)/arch/arm/boot/scsi_wait_scan.ko; /media/root/robcore/AIK/S4-Machinex-6.x-new/system/lib;
+rm /media/root/robcore/AIK/split_img/boot.img-zImage;
+cp $(pwd)/arch/arm/boot/boot.img-zImage /media/root/robcore/AIK/split_img;
+sh /media/root/robcore/AIK/repackimg.sh;
+cp image-new.img /media/root/robcore/AIK/S4-Machinex-6.x-new/boot.img
--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -40,9 +40,6 @@
 #ifdef CONFIG_SND_SOC_ES325
 #include "es325-export.h"
 #endif
-#ifdef CONFIG_BOEFFLA_SOUND
-#include "boeffla_sound.h"
-#endif
 
 static int cfilt_adjust_ms = 10;
 module_param(cfilt_adjust_ms, int, 0644);
@@ -4104,13 +4101,11 @@ extern unsigned int snd_hax_cache_read(unsigned int);
 extern void snd_hax_cache_write(unsigned int, unsigned int);
 #endif
 
-#if defined(CONFIG_SOUND_CONTROL_HAX_3_GPL) || defined(CONFIG_BOEFFLA_SOUND)
+#ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL
+static
+#endif
 int tabla_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
-#else	
-static int tabla_write(struct snd_soc_codec *codec, unsigned int reg,
-	unsigned int value)
-#endif	
 {
 	int ret;
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
@@ -4118,11 +4113,6 @@ static int tabla_write(struct snd_soc_codec *codec, unsigned int reg,
 #endif
 
 	BUG_ON(reg > TABLA_MAX_REGISTER);
-
-#ifdef CONFIG_BOEFFLA_SOUND
-	// Boeffla Sound write hook
-	value = boeffla_sound_hook_tabla_write(reg, value);
-#endif
 
 	if (!tabla_volatile(codec, reg)) {
 		ret = snd_soc_cache_write(codec, reg, value);
@@ -4144,37 +4134,15 @@ static int tabla_write(struct snd_soc_codec *codec, unsigned int reg,
 	return wcd9xxx_reg_write(codec->control_data, reg, value);
 #endif
 }
-#if defined(CONFIG_SOUND_CONTROL_HAX_3_GPL) || defined(CONFIG_BOEFFLA_SOUND)
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 EXPORT_SYMBOL(tabla_write);
 #endif
 
-#ifdef CONFIG_BOEFFLA_SOUND
-int tabla_write_no_hook(struct snd_soc_codec *codec, unsigned int reg,
-	unsigned int value)
-{
-	int ret;
-	
-	BUG_ON(reg > TABLA_MAX_REGISTER);
-	
-	if (!tabla_volatile(codec, reg)) {
-		ret = snd_soc_cache_write(codec, reg, value);
-		if (ret != 0)
-			dev_err(codec->dev, "Cache write to %x failed: %d\n",
-				reg, ret);
-	}
-
-	return wcd9xxx_reg_write(codec->control_data, reg, value);
-}
-EXPORT_SYMBOL(tabla_write_no_hook);
+#ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL
+static
 #endif
-
-#if defined(CONFIG_SOUND_CONTROL_HAX_3_GPL) || defined(CONFIG_BOEFFLA_SOUND)
 unsigned int tabla_read(struct snd_soc_codec *codec,
 				unsigned int reg)
-#else
-static unsigned int tabla_read(struct snd_soc_codec *codec,
-				unsigned int reg)
-#endif
 {
 	unsigned int val;
 	int ret;
@@ -4194,7 +4162,7 @@ static unsigned int tabla_read(struct snd_soc_codec *codec,
 	val = wcd9xxx_reg_read(codec->control_data, reg);
 	return val;
 }
-#if defined(CONFIG_SOUND_CONTROL_HAX_3_GPL) || defined(CONFIG_BOEFFLA_SOUND)
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 EXPORT_SYMBOL(tabla_read);
 #endif
 
@@ -8979,11 +8947,6 @@ static int tabla_codec_probe(struct snd_soc_codec *codec)
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 	pr_info("tabla codec probe...\n");
 	fauxsound_codec_ptr = codec;
-#endif
-
-#ifdef CONFIG_BOEFFLA_SOUND
-	// Boeffla Sound probe hook
-	boeffla_sound_hook_tabla_codec_probe(codec);
 #endif
 
 	codec->control_data = dev_get_drvdata(codec->dev->parent);
