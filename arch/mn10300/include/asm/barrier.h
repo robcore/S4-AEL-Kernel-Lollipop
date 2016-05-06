218e21182298854ef8bd482070f53e4293eb0f04
robcore <robpatershuk@gmail.com>
2016-05-06 16:16:31 -0600

    there

--- arch/mn10300/include/asm/barrier.h
+++ arch/mn10300/include/asm/barrier.h
@@ -1,7 +1,4 @@
-/* Generic barrier definitions, based on MN10300 definitions.
- *
- * It should be possible to use these on really simple architectures,
- * but it serves more as a starting point for new ports.
+/* MN10300 memory barrier definitions
  *
  * Copyright (C) 2007 Red Hat, Inc. All Rights Reserved.
  * Written by David Howells (dhowells@redhat.com)
@@ -11,20 +8,10 @@
  * as published by the Free Software Foundation; either version
  * 2 of the Licence, or (at your option) any later version.
  */
-#ifndef __ASM_GENERIC_BARRIER_H
-#define __ASM_GENERIC_BARRIER_H
+#ifndef _ASM_BARRIER_H
+#define _ASM_BARRIER_H
 
-#ifndef __ASSEMBLY__
-
-#define nop() asm volatile ("nop")
-
-/*
- * Force strict CPU ordering.
- * And yes, this is required on UP too when we're talking
- * to devices.
- *
- * This implementation only contains a compiler barrier.
- */
+#define nop()	asm volatile ("nop")
 
 #define mb()	asm volatile ("": : :"memory")
 #define rmb()	mb()
@@ -34,17 +21,17 @@
 #define smp_mb()	mb()
 #define smp_rmb()	rmb()
 #define smp_wmb()	wmb()
-#else
+#define set_mb(var, value)  do { xchg(&var, value); } while (0)
+#else  /* CONFIG_SMP */
 #define smp_mb()	barrier()
 #define smp_rmb()	barrier()
 #define smp_wmb()	barrier()
-#endif
-
 #define set_mb(var, value)  do { var = value;  mb(); } while (0)
+#endif /* CONFIG_SMP */
+
 #define set_wmb(var, value) do { var = value; wmb(); } while (0)
 
 #define read_barrier_depends()		do {} while (0)
 #define smp_read_barrier_depends()	do {} while (0)
 
-#endif /* !__ASSEMBLY__ */
-#endif /* __ASM_GENERIC_BARRIER_H */
+#endif /* _ASM_BARRIER_H */
