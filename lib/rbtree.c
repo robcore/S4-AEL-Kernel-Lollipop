2abc42986ce91045e9d6c52cecfcf49c57906ef7
robcore <robpatershuk@gmail.com>
2016-05-11 21:50:52 -0600

    still workin

--- lib/rbtree.c
+++ lib/rbtree.c
@@ -23,24 +23,6 @@
 #include <linux/rbtree.h>
 #include <linux/export.h>
 
-#define	RB_RED		0
-#define	RB_BLACK	1
-
-#define rb_color(r)   ((r)->__rb_parent_color & 1)
-#define rb_is_red(r)   (!rb_color(r))
-#define rb_is_black(r) rb_color(r)
-#define rb_set_red(r)  do { (r)->__rb_parent_color &= ~1; } while (0)
-#define rb_set_black(r)  do { (r)->__rb_parent_color |= 1; } while (0)
-
-static inline void rb_set_parent(struct rb_node *rb, struct rb_node *p)
-{
-	rb->__rb_parent_color = rb_color(rb) | (unsigned long)p;
-}
-static inline void rb_set_color(struct rb_node *rb, int color)
-{
-	rb->__rb_parent_color = (rb->__rb_parent_color & ~1) | color;
-}
-
 static void __rb_rotate_left(struct rb_node *node, struct rb_root *root)
 {
 	struct rb_node *right = node->rb_right;
@@ -273,7 +255,7 @@ void rb_erase(struct rb_node *node, struct rb_root *root)
 			rb_set_parent(old->rb_right, node);
 		}
 
-		node->__rb_parent_color = old->__rb_parent_color;
+		node->rb_parent_color = old->rb_parent_color;
 		node->rb_left = old->rb_left;
 		rb_set_parent(old->rb_left, node);
 
@@ -405,7 +387,7 @@ struct rb_node *rb_next(const struct rb_node *node)
 {
 	struct rb_node *parent;
 
-	if (RB_EMPTY_NODE(node))
+	if (rb_parent(node) == node)
 		return NULL;
 
 	/* If we have a right-hand child, go down and then left as far
@@ -434,7 +416,7 @@ struct rb_node *rb_prev(const struct rb_node *node)
 {
 	struct rb_node *parent;
 
-	if (RB_EMPTY_NODE(node))
+	if (rb_parent(node) == node)
 		return NULL;
 
 	/* If we have a left-hand child, go down and then right as far
